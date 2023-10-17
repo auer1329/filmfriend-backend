82bbf538c57e9572802ee747c57c843886522088
Théo Thoumine <theo@thoumine.de>
2023-10-17 14:50:39 +0200

    initial

---
+++ src/main/java/webtech/filmfriend/FilmfriendApplication.java
@@ -0,0 +1,13 @@
+package webtech.filmfriend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmfriendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmfriendApplication.class, args);
+	}
+
+}
