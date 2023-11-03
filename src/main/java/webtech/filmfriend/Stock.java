bcde58ab4f5812a37a48b5e3703d3b21f1d4aebc
Théo Thoumine <theo@thoumine.de>
2023-11-03 23:06:37 +0100

    adds stock entity
    Co-authored-by: Aaron Ziegler <0588522@htw-berlin.de>

---
+++ src/main/java/webtech/filmfriend/Stock.java
@@ -0,0 +1,136 @@
+package webtech.filmfriend;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+@Entity
+public class Stock {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String apiID;
+    private String brand;
+    private String name;
+    private int iso;
+    private boolean formatThirtyFive;
+    private boolean formatOneTwenty;
+    private boolean color;
+    private String process;
+    private String staticImageUrl;
+    private String description;
+    @OneToMany
+    private List<Feature> keyFeatures;
+
+    public Stock() {}
+
+    public Stock(String id, String brand, String name, int iso, boolean formatThirtyFive, boolean formatOneTwenty) {
+        this.apiID = id;
+        this.brand = brand;
+        this.name = name;
+        this.iso = iso;
+        this.formatThirtyFive = formatThirtyFive;
+        this.formatOneTwenty = formatOneTwenty;
+    }
+
+    public Stock(String id, String brand, String name, int iso, boolean formatThirtyFive, boolean formatOneTwenty, boolean color, String process, String staticImageUrl, String description, List<Feature> keyFeatures) {
+        this.apiID = id;
+        this.brand = brand;
+        this.name = name;
+        this.iso = iso;
+        this.formatThirtyFive = formatThirtyFive;
+        this.formatOneTwenty = formatOneTwenty;
+        this.color = color;
+        this.process = process;
+        this.staticImageUrl = staticImageUrl;
+        this.description = description;
+        this.keyFeatures = keyFeatures;
+    }
+
+    public String getApiID() {
+        return apiID;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getIso() {
+        return iso;
+    }
+
+    public boolean isFormatThirtyFive() {
+        return formatThirtyFive;
+    }
+
+    public boolean isFormatOneTwenty() {
+        return formatOneTwenty;
+    }
+
+    public boolean isColor() {
+        return color;
+    }
+
+    public String getProcess() {
+        return process;
+    }
+
+    public String getStaticImageUrl() {
+        return staticImageUrl;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public List<Feature> getKeyFeatures() {
+        return keyFeatures;
+    }
+
+    public void setApiID(String apiID) {
+        this.apiID = apiID;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIso(int iso) {
+        this.iso = iso;
+    }
+
+    public void setFormatThirtyFive(boolean formatThirtyFive) {
+        this.formatThirtyFive = formatThirtyFive;
+    }
+
+    public void setFormatOneTwenty(boolean formatOneTwenty) {
+        this.formatOneTwenty = formatOneTwenty;
+    }
+
+    public void setColor(boolean color) {
+        this.color = color;
+    }
+
+    public void setProcess(String process) {
+        this.process = process;
+    }
+
+    public void setStaticImageUrl(String staticImageUrl) {
+        this.staticImageUrl = staticImageUrl;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setKeyFeatures(List<Feature> keyFeatures) {
+        this.keyFeatures = keyFeatures;
+    }
+}
