802debf952c5d39e9f01bcd340877122a815768c
ZieglerAaron <s0588522@htw-berlin.de>
2023-12-05 18:51:21 +0100

    Allow for CORS

--- src/main/java/webtech/filmfriend/StockController.java
+++ src/main/java/webtech/filmfriend/StockController.java
@@ -8,15 +8,18 @@ public class StockController {
 
     @Autowired
     StockService service;
+    @CrossOrigin
     @PostMapping("/api/stocks")
     public Stock createStock(@RequestBody Stock stock) {
         return service.save(stock);
     }
+    @CrossOrigin
     @GetMapping("/api/stocks/{id}")
     public Stock getStock(@PathVariable String id) {
         Long stockId = Long.parseLong(id);
         return service.get(stockId);
     }
+    @CrossOrigin
     @GetMapping("/api/stocks")
     public Iterable<Stock> getAllStocks() {
         return service.getAll();
