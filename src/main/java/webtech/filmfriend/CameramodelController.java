f35db9bb7e7f14922f1568ff0362a015a083f4a4
ZieglerAaron <s0588522@htw-berlin.de>
2023-12-05 18:54:13 +0100

    Allow for CORS
    Co-authored-by: TheoThmn <s0586704@htw-berlin.de>

--- src/main/java/webtech/filmfriend/CameramodelController.java
+++ src/main/java/webtech/filmfriend/CameramodelController.java
@@ -8,16 +8,18 @@ public class CameramodelController {
 
     @Autowired
     CameramodelService cameramodelService;
-
+    @CrossOrigin
     @PostMapping("/api/cameramodel")
     public Cameramodel saveCameramodel(@RequestBody Cameramodel cameramodel) {
         return cameramodelService.save(cameramodel);
     }
+    @CrossOrigin
 
     @GetMapping("/api/cameramodel")
     public Iterable<Cameramodel> getAllCameramodels() {
         return cameramodelService.getAll();
     }
+    @CrossOrigin
 
     @GetMapping("/api/cameramodel/{id}")
     public Cameramodel getCameramodel(@PathVariable String id) {
