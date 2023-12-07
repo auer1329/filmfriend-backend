9ed3fc39a9d73c6fa1da253668609557c61285b9
ZieglerAaron <s0588522@htw-berlin.de>
2023-12-07 22:11:39 +0100

    Add getters and setters to Cameramodel.java
    Co-authored-by: TheoThmn <s0586704@htw-berlin.de>

--- src/main/java/webtech/filmfriend/Cameramodel.java
+++ src/main/java/webtech/filmfriend/Cameramodel.java
@@ -15,4 +15,60 @@ public class Cameramodel {
     private boolean formatThirtyFive;
     private boolean formatOneTwenty;
     public Cameramodel() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getStaticImageUrl() {
+        return staticImageUrl;
+    }
+
+    public void setStaticImageUrl(String staticImageUrl) {
+        this.staticImageUrl = staticImageUrl;
+    }
+
+    public boolean isFormatThirtyFive() {
+        return formatThirtyFive;
+    }
+
+    public void setFormatThirtyFive(boolean formatThirtyFive) {
+        this.formatThirtyFive = formatThirtyFive;
+    }
+
+    public boolean isFormatOneTwenty() {
+        return formatOneTwenty;
+    }
+
+    public void setFormatOneTwenty(boolean formatOneTwenty) {
+        this.formatOneTwenty = formatOneTwenty;
+    }
 }
