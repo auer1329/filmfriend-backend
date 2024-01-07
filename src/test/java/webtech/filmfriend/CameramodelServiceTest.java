e637965b3bbe98b43c53a35ae4f2bf4aff24c109
Théo Thoumine <theo@thoumine.de>
2024-01-07 15:58:18 +0100

    Add test to get()
    Co-authored-by: ZieglerAaron <s0588522@htw-berlin.de>

---
+++ src/test/java/webtech/filmfriend/CameramodelServiceTest.java
@@ -0,0 +1,44 @@
+package webtech.filmfriend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doReturn;
+
+@SpringBootTest
+class CameramodelServiceTest {
+
+    @Autowired
+    CameramodelService cameramodelService;
+
+    @MockBean
+    CameramodelRepo cameramodelRepo;
+
+    @Test
+    void get() {
+        var cameramodelNikon = new Cameramodel();
+        var cameramodelCanon = new Cameramodel();
+
+        cameramodelNikon.setId(1L);
+        cameramodelNikon.setBrand("Nikon");
+        cameramodelNikon.setModel("F3");
+        cameramodelNikon.setFormatThirtyFive(true);
+        cameramodelNikon.setFormatOneTwenty(false);
+
+        cameramodelCanon.setId(2L);
+        cameramodelCanon.setBrand("Canon");
+        cameramodelCanon.setModel("AE-1");
+        cameramodelCanon.setFormatThirtyFive(true);
+        cameramodelCanon.setFormatOneTwenty(false);
+
+        doReturn(Optional.of(cameramodelNikon)).when(cameramodelRepo).findById(1L);
+        doReturn(Optional.of(cameramodelCanon)).when(cameramodelRepo).findById(2L);
+
+        assertEquals(cameramodelCanon.getBrand(), cameramodelService.get(2L).getBrand());
+    }
+}
